a3c4bc70a29d3a3cd0c9ef68efb393ccea5f2445
dsal0506 <dsalinas_5605@stu.southtexascollege.edu>
2023-12-12 13:14:20 -0600

    added tables and teams

---
+++ prisma/migrations/20231212190522_inital/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Team" (
+    "teamId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "club" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "stadium" TEXT NOT NULL,
+    "coach" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Team_pkey" PRIMARY KEY ("teamId")
+);
---
+++ src/models/teams.js
@@ -0,0 +1,30 @@
+import db from '../utils/db'
+
+export const getTeams = async (skip, take) => {
+  const count = await db.team.count()
+  const teams = await db.team.findMany({
+    skip,
+    take,
+  })
+  return { count, teams }
+}
+
+export const getTeams = async (id) =>
+  db.team.findUnique({ where: { teamId: id } })
+
+export const addTeam = async (teamData) =>
+  db.team.create({ data: { ...teamData } })
+
+export const updateTeam = async (id, teamData) => {
+  const team = await getTeam(id)
+  if (team) {
+    return db.team.update({
+      where: { teamIdId: id },
+      data: { ...teamDataData },
+    })
+  }
+  return null
+}
+
+export const deleteTeam = async (id) =>
+  db.team.delete({ where: { teamId: id } })
